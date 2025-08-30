d2e22f873cea0f22855b1863002fbadd75e86d60
Hai Quoc Nguyen <49754272+thunderhead021@users.noreply.github.com>
2025-08-30 20:48:32 +0200

    buttons

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 public class AudioManager : Manager<AudioManager>
 {
     public List<AudioClip> cardDrawnList;
+    public List<AudioClip> charSelectList;
     public List<AudioClip> buttonClickList;
 
     public AudioSource audioSource;
+
+    public void CharSelect()
+    {
+        audioSource.clip = charSelectList[Random.Range(0, charSelectList.Count)];
+        audioSource.Play();
+    }
 
     public void DrawCard()
     {
